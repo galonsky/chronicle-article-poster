c3232f2ab6640aac5006fb57ca645bc56f6fc172
Trevor Reid <trevorgreid@gmail.com>
2011-04-05 20:47:30 -0400

    switched over to express

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 var formidable = require('formidable'),
-http = require('http'),
+http = require('http'), 
+sys = require('sys'),
+express = require('express'),
+stylus = require('stylus');
 
-sys = require('sys');
+var app = express.createServer();
+app.configure(function(){ 
+  app.set('view engine','jade');
+  app.set('views', __dirname+'/views');
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.logger());
+  app.use(app.router);
+  app.use(express.static(__dirname+'/public'));
+  app.use(stylus.middleware({src: __dirname + '/public'}));
+});
 
-http.createServer(function(req, res) {
-  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-    // parse a file upload
-    var form = new formidable.IncomingForm();
-    form.uploadDir='./zipfiles';
-    form.keepExtensions=true;
-    form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.write(sys.inspect({fields: fields, files: files})+'\n');
-var exec = require('child_process').exec;
-      function puts (error, stdout, stderr){ 
-        res.end('Unzip Results: \n' + stdout);
-        console.log('stdout: ' + stdout);
-      }
-      exec("unzip zipfiles/*.zip", puts);
-
-    });
-
-    return;
-  }
-
-  // show a file upload form
+app.get('/', function(req, res){
   res.writeHead(200, {'content-type': 'text/html'});
   res.end(
     '<form action="/upload" enctype="multipart/form-data" method="post">'+ 
@@ -34,7 +25,28 @@ var exec = require('child_process').exec;
           '<input type="submit" value="Upload">'+
             '</form>'
   );
+});
+
+app.post('/upload', function(req,res){
+  var form = new formidable.IncomingForm();
+  form.uploadDir='./zipfiles';
+  form.keepExtensions=true;
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received upload:\n\n');
+    res.write(sys.inspect({fields: fields, files: files})+'\n');
+    var exec = require('child_process').exec;
+    function puts (error, stdout, stderr){ 
+      res.end('Unzip Results: \n' + stdout);
+      console.log('stdout: ' + stdout);
+    }
+    exec("unzip zipfiles/*.zip", puts);
+
+  });
+
+});
 
 
-}).listen(8124, "127.0.0.1");
+
+app.listen(8124, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:8124/');
