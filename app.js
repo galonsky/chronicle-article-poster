6af10c0bec9f1e5925a7f5670420ee76628d14bb
Alex Galonsky <alex@galonsky.com>
2011-04-07 22:28:56 -0400

    Rendering new view

--- app.js
+++ app.js
@@ -17,14 +17,15 @@ app.configure(function(){
 });
 
 app.get('/', function(req, res){
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.end(
-    '<form action="/upload" enctype="multipart/form-data" method="post">'+ 
-      '<input type="text" name="title"><br>' +
-        '<input type="file" name="upload" multiple="multiple"><br>'+
-          '<input type="submit" value="Upload">'+
-            '</form>'
-  );
+  // res.writeHead(200, {'content-type': 'text/html'});
+  // res.end(
+  //   '<form action="/upload" enctype="multipart/form-data" method="post">'+ 
+  //     '<input type="text" name="title"><br>' +
+  //       '<input type="file" name="upload" multiple="multiple"><br>'+
+  //         '<input type="submit" value="Upload">'+
+  //           '</form>'
+  // );
+  res.render('main');
 });
 
 app.post('/upload', function(req,res){
